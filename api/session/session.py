85d87105927a128a9a40be1f71671b76813412d1
Steven Atkinson <steven.atkinsoN@gmail.com>
2023-10-09 03:40:48 -0700

    Session layer classes made concise

---
+++ api/session/session.py
@@ -0,0 +1,46 @@
+from typing import Optional
+
+from api.client.core_client_interface import CoreClientInterface
+from api.client.user import User
+from api.client.public_client import PublicClient
+from api.client.private_client import PrivatePromptClient
+
+
+class PublicUserSession:
+    client_cls = PublicClient
+
+    def __init__(self, user: Optional[User] = None):
+        self.client: CoreClientInterface = self.client_cls(user)
+
+    async def add_prompt(self, prompt_data: dict[str, any]) -> str:
+        return await self.client.add_prompt(prompt_data)
+
+    async def list_prompts(self, skip: int = 0, limit: int = 10) -> list[dict]:
+        return await self.client.list_prompts(skip, limit)
+
+    async def delete_prompt(self, guid: str) -> None:
+        await self.client.delete_prompt(guid)
+
+    async def update_prompt(self, guid: str, updated_data: dict[str, any]) -> None:
+        await self.client.update_prompt(guid, updated_data)
+
+    async def get_prompt(self, guid: str) -> dict[str, any]:
+        return await self.client.get_prompt(guid)
+
+    async def search_prompts(self, query: str) -> list[dict]:
+        return await self.client.search_prompts(query)
+
+    async def list_prompts_by_tags(self, tags: list[str]) -> list[dict]:
+        return await self.client.list_prompts_by_tags(tags)
+
+    async def list_prompts_by_classification(self, classification: str) -> list[dict]:
+        return await self.client.list_prompts_by_classification(classification)
+
+
+class PrivateUserSession(PublicUserSession):
+    client_cls = PrivatePromptClient
+
+    def __init__(self, user: User):
+        if not user:
+            raise ValueError("A valid user object is required for PrivateUserSession.")
+        super().__init__(user)
